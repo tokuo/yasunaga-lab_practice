a9e4a6e1983c7d0cd29756f6933a32e6d203075a
tokuo <shgtkmt@gmail.com>
2017-06-08 07:33:56 +0900

    Investigation finished

--- diff/diff.py
+++ diff/diff.py
@@ -1,15 +1,26 @@
 import sys
+import struct
 """import tensorflow as tf"""
 
 class tfdif:
     def __init__(self):
         self.dif = ''
+        self.elength = ''
+        self.start = ''
         
     def difget(self,dimage):
         self.dif = open(dimage,'rb').read()
+        if (self.dif[0] != 77) and (self.dif[1] != 77):
+            print('this image is not diff!')
+            return
+        self.start = struct.unpack('>i',self.dif[4:8])[0]
+        self.elength = struct.unpack('>h',self.dif[self.start:self.start+2])[0]
+        '''for i in range(self.elength):'''
+            
         
     """def"""
     
 demo = tfdif()
 demo.difget('demo.tif')
-print(demo.dif)
+
+print(demo.elength)
